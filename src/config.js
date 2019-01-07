1fb51bcc74164e4e86fa7a4263f5f9f4b857a65d
GhostPencil <ljfanizza@gmail.com>
2019-01-07 18:31:18 -0500

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
+const dev = {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-2-api-dev-attachmentsbucket-1tcgkvjh8zv5z"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://9mjd4roqe0.execute-api.us-east-1.amazonaws.com/dev"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_4k3yJaVE6",
+        APP_CLIENT_ID: "2c74pq0ds0ps7o4he3jalttgd5",
+        IDENTITY_POOL_ID: "us-east-1:4aa7dd88-0d8b-4e10-b9d3-756361a8fde9"
+    }
+};
+
+const prod = {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-2-api-prod-attachmentsbucket-u4uzp5n7n5s8"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://vaien8ovz7.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_qwaESmRhf",
+        APP_CLIENT_ID: "uiid08eu06jo9kbi7rb7jp3n3",
+        IDENTITY_POOL_ID: "us-east-1:221137ae-bf8e-4e9e-8cde-0b1d0b1b0124"
+    }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
 export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
-  s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
-  },
-  apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
-  },
-  cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
-  }
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
 };
